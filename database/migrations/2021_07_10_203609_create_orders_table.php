6f4612dfef7f1ecd4abcdb37f9505aa96cbf5994
linojackson <jackson.lino@hotmail.com>
2021-07-12 22:07:42 -0300

    initial commit

---
+++ database/migrations/2021_07_10_203609_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user');
+            $table->integer('item_amount');
+            $table->integer('price');
+            $table->integer('status');
+            $table->timestamps();
+
+            $table->foreign('user')->references('id')->on('users')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/migrations/2021_07_11_023925_create_orders_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders_products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('order');
+            $table->unsignedBigInteger('product');
+            $table->timestamps();
+
+            $table->foreign('product')->references('id')->on('products')->onDelete('CASCADE');
+            $table->foreign('order')->references('id')->on('orders')->onDelete('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders_products');
+    }
+}
